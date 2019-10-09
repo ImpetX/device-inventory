09e2eca1977a476c2c462f4664c08bdaf1187892
ksharifbd <ksharifbd@gmail.com>
2019-10-09 11:13:34 +0600

    DI-7:: uses css baseline

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 
 const App = () => (
-  <Button variant="contained" color="primary">
-    Hello World
-  </Button>
+  <Box>
+    <CssBaseline />
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </Box>
 );
 
 export default App;
