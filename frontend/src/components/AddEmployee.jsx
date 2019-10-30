83c07dd84da17b23fce9fcfd0d396a1c488b9bc6
ksharifbd <ksharifbd@gmail.com>
2019-10-30 11:08:10 +0600

    DI-21:: Starts using TextField

--- frontend/src/components/AddEmployee.jsx
+++ frontend/src/components/AddEmployee.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 import { PageHeader } from './lib';
 
-const AddEmployee = () => <PageHeader headerText="Add Employee" />;
+const AddEmployee = () => (
+  <>
+    <PageHeader headerText="Add Employee" />
+    <form>
+      <TextField required id="name" name="name" label="Name" fullWidth />
+    </form>
+  </>
+);
 
 export default AddEmployee;
