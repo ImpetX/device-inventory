a549856099f8f7bcd526d734b593cf0d18c48e65
ksharifbd <ksharifbd@gmail.com>
2019-11-01 10:55:57 +0600

    DI-21:: attempt at using date picker

--- frontend/src/components/AddEmployee.jsx
+++ frontend/src/components/AddEmployee.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
+import MenuItem from '@material-ui/core/MenuItem';
 import { PageHeader, Form } from './lib';
+
+const genders = [
+  {
+    label: 'male',
+    value: 'male',
+  },
+
+  {
+    label: 'female',
+    value: 'female',
+  },
+];
 
 const AddEmployee = () => {
   const marginBottom = 1.25;
@@ -11,7 +24,14 @@ const AddEmployee = () => {
       <PageHeader headerText="Add Employee" />
       <Form onSubmit={() => 'form submitted'}>
         <Box mb={marginBottom}>
-          <TextField required id="name" name="name" label="Name" fullWidth />
+          <TextField
+            required
+            type="text"
+            id="name"
+            name="name"
+            label="Name"
+            fullWidth
+          />
         </Box>
         <Box mb={marginBottom}>
           <TextField
@@ -20,6 +40,42 @@ const AddEmployee = () => {
             id="phone"
             name="phone"
             label="Phone"
+            fullWidth
+          />
+        </Box>
+        <Box mb={marginBottom}>
+          <TextField
+            required
+            type="date"
+            id="bithdate"
+            name="birthdate"
+            label="Birthdate"
+            fullWidth
+          />
+        </Box>
+        <Box mb={marginBottom}>
+          <TextField
+            required
+            select
+            id="gender"
+            name="gender"
+            label="Gender"
+            fullWidth
+          >
+            {genders.map(gender => (
+              <MenuItem key={gender.label} value={gender.value}>
+                {gender.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+        <Box mb={marginBottom}>
+          <TextField
+            required
+            type="date"
+            id="joiningDate"
+            name="joiningdate"
+            label="Joining Date"
             fullWidth
           />
         </Box>
