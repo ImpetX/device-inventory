59252bb9dceadc9e0248301d4f3d6ba88f37cc69
ksharifbd <ksharifbd@gmail.com>
2019-11-01 10:05:06 +0600

    DI-21:: uses box for spacing

--- frontend/src/components/AddEmployee.jsx
+++ frontend/src/components/AddEmployee.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
 import { PageHeader, Form } from './lib';
 
-const AddEmployee = () => (
-  <>
-    <PageHeader headerText="Add Employee" />
-    <Form onSubmit={() => 'form submitted'}>
-      <TextField required id="name" name="name" label="Name" fullWidth />
-    </Form>
-  </>
-);
+const AddEmployee = () => {
+  const marginBottom = 1.25;
+
+  return (
+    <>
+      <PageHeader headerText="Add Employee" />
+      <Form onSubmit={() => 'form submitted'}>
+        <Box mb={marginBottom}>
+          <TextField required id="name" name="name" label="Name" fullWidth />
+        </Box>
+        <Box mb={marginBottom}>
+          <TextField
+            required
+            type="tel"
+            id="phone"
+            name="phone"
+            label="Phone"
+            fullWidth
+          />
+        </Box>
+      </Form>
+    </>
+  );
+};
 
 export default AddEmployee;
