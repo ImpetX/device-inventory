f6ed87240d08a47d43693f9f5baa119fe98161a4
ksharifbd <ksharifbd@gmail.com>
2019-11-12 10:35:05 +0600

    DI-29:: uses react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import makeStyles from '@material-ui/styles/makeStyles';
-// eslint-disable-next-line import/named
-import { DetailsEmployee } from './components/employee';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  ListEmployee,
+  AddEmployee,
+  DetailsEmployee,
+} from './components/employee';
+import { ListDevice, AddDevice, DetailsDevice } from './components/device';
 import { containerSpacing } from './tokens';
 import { convertToRem } from './utils';
 
@@ -18,12 +23,29 @@ const App = () => {
   const classes = useStyles();
 
   return (
-    <>
+    <Router>
       <CssBaseline />
       <div className={classes.container}>
-        <DetailsEmployee />
+        <Route exact path="/devices">
+          <ListDevice />
+        </Route>
+        <Route exact path="/device/add">
+          <AddDevice />
+        </Route>
+        <Route exact path="/devices/:id">
+          <DetailsDevice />
+        </Route>
+        <Route exact path="/employees">
+          <ListEmployee />
+        </Route>
+        <Route exact path="/employee/add">
+          <AddEmployee />
+        </Route>
+        <Route exact path="/employees/:id">
+          <DetailsEmployee />
+        </Route>
       </div>
-    </>
+    </Router>
   );
 };
 
