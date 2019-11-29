b4998a640ccbcf04a56925910d10899cf5b73be2
ksharifbd <ksharifbd@gmail.com>
2019-11-29 17:37:23 +0600

    DI-39:: uses better types

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,9 +1,15 @@
 import dotenv from 'dotenv';
-import mysql, { PoolOptions } from 'mysql2/promise';
+import mysql, { Pool, PoolOptions } from 'mysql2/promise';
 
 dotenv.config();
 
 class Db {
+  /*
+    for '!' sign view
+    https://github.com/Microsoft/TypeScript-Vue-Starter/issues/36#issuecomment-371434263
+  */
+  private pool!: Pool;
+
   private config: PoolOptions = {
     database: 'device_inventory',
     host: process.env.MYSQL_HOST,
@@ -12,10 +18,10 @@ class Db {
     user: process.env.MYSQL_USER,
   };
 
-  public async connect() {
-    const pool = mysql.createPool(this.config);
+  public async connect(): Promise<Pool> {
+    this.pool = mysql.createPool(this.config);
 
-    return pool;
+    return this.pool;
   }
 }
 
